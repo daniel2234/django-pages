37fa438eb362b4b5156a980ad1f842e4404eb2dd
daniel2234 <kwiatkowski.daniel@gmail.com>
2022-03-08 09:16:15 -0500

    New updates for Heroku deployment

--- pages/views.py
+++ pages/views.py
@@ -9,5 +9,5 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
-class AboutPageView(TemplateView): #new
+class AboutPageView(TemplateView): # new
     template_name = "about.html"
